d8002be122bdcd7e9231ea0588d99cacc8657733
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-06 00:42:30 +0530

    data and value

--- Module37/13feb/server.js
+++ Module37/13feb/server.js
@@ -1,2 +1,23 @@
 console.log("Shourya")
 console.log(this)
+
+//Assignment Operator
+let highScore = 300;
+console.log(highScore);
+
+highScore = 400;
+console.log(highScore);
+
+//Array
+let name = ["Shourya", 'Dubey', 1, 3, true];
+console.log(name);
+
+
+let pwCoursePrice = 200;
+let gstOnCourse = 30;
+let finalPurchaseAmount = pwCoursePrice + gstOnCourse;
+console.log("Final Amount to be Paid: ",finalPurchaseAmount);
+
+let pwDsaCourse = 5000;
+console.log("Final Price of DSA Course: ", pwCoursePrice + gstOnCourse);
+
