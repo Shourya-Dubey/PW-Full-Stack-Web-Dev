ea02c823ccc79be5edc090f2f92dc65c0f8d5d2e
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-07 18:48:33 +0530

    function

---
+++ Module38/function.js
@@ -0,0 +1,16 @@
+function name(){
+    console.log("Hi there")
+    console.log("How are you");
+}
+name();
+
+function sqr(x){
+    console.log(x*x);
+}
+sqr(8)
+
+function sum(a, b){
+    return a+b;
+}
+let add = sum(5,5);
+console.log(add);
