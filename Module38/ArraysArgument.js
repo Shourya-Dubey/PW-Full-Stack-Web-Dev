395e73a836df2f928af303f890b42e45f4969288
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-07 22:02:05 +0530

    Arrays as an Argument

---
+++ Module38/ArraysArgument.js
@@ -0,0 +1,15 @@
+function sum([num1, num2]){
+    return num1 + num2;
+}
+let number = [10, 12];
+console.log(sum(number));
+
+
+
+function add([num1, num2, num3]){
+    return num1 + num2 + num3;
+}
+let addNumber = [1, 2, 3, 4, 5];
+console.log(add(addNumber));
+console.log(addNumber);
+
