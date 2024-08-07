3f1d6e8f8624c9e4e7c893d8410bda30d241c2e3
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-07 18:26:55 +0530

    Method of Array

---
+++ Module38/method.js
@@ -0,0 +1,31 @@
+let a = [1,2,3,4];
+a.push(5,6);
+console.log(a);
+
+a.pop();
+console.log(a);
+
+a.shift();
+console.log(a);
+
+a.unshift(1);
+console.log(a);
+
+let b = [6,7,8];
+let c = [9,10,11];
+let d = b.concat(c);
+console.log(d);
+
+let name = ["S","h","o","u","r","y","a"];
+let e = name.join(" ");
+console.log(e);
+
+name.reverse();
+console.log(name);
+
+console.log(name.indexOf("o"));
+
+console.log(name.slice(1,4));
+
+// splice
+
