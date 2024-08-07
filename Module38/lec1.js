51b70d9ef95927c2098e8a4d06d77ab68b3c76f6
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-07 18:10:14 +0530

    Array

---
+++ Module38/lec1.js
@@ -0,0 +1,13 @@
+let a = [1,2,3,4,"Shourya", true];
+console.log(a);
+
+let b = new Array();
+console.log(b);
+
+let c = new Array(5);
+console.log(c);
+
+console.log(a[1]);
+a[3] = 5;
+console.log(a);
+
