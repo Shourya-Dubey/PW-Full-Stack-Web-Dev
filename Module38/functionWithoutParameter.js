09478cf483f6f0cb4a07ca004e80795aa91284b5
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-07 21:53:11 +0530

    function with no parameter

---
+++ Module38/functionWithoutParameter.js
@@ -0,0 +1,16 @@
+function message(){
+    console.log("Hello Shourya Dubey");
+}
+message();
+
+function sayMessage(){
+    return "I am Happy";
+}
+let msg = sayMessage();
+console.log(msg);
+
+function textMessage(){
+    return "Today is Wednesday";
+}
+let newMessage = msg + textMessage();
+console.log(newMessage);
