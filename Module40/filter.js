c3d41d7d03b409aa52539803ca4a26f1394188fd
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-10 14:13:05 +0530

    add something to filter.js

--- Module40/filter.js
+++ Module40/filter.js
@@ -1,6 +1,10 @@
 const heros = ["nagraj", "doga", "dhruva", "maniraj"];
 console.log(heros)
+
 const herosWithRaj = heros.filter(function(h){
     return h.endsWith("raj")
 })
+
+// const herosWithRaj = heros.filter((h) => h.endsWith("raj"))
+
 console.log(herosWithRaj);
