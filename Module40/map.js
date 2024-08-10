f4c7faf658cb7812d33d9296e287f29436bdbaec
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-10 14:04:15 +0530

    map

---
+++ Module40/map.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3]
+arr.map(function(el, index, arr){
+    console.log(el, index, arr);
+
+})
+
+const heros = ["nagraj", "doga", "dhruva", "maniraj"]
+heros.map((el)=>{
+    console.log(el.toUpperCase());
+})
