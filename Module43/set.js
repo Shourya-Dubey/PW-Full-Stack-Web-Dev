a47fce831b764048e81562811d51e3d357b2568c
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-11 18:41:13 +0530

    setDifference

--- Module43/set.js
+++ Module43/set.js
@@ -12,3 +12,21 @@ console.log(set.delete(2))
 console.log(set);
 
 console.log(set.size);
+
+
+function setDifference(setA, setB){
+    return new Set([...setA].filter(ele => !setB.has(ele)))
+}
+
+let setA = new Set()
+setA.add(1)
+setA.add(2)
+setA.add(3)
+
+let setB = new Set()
+setB.add(4)
+setB.add(5)
+setB.add(1)
+
+let setResult = setDifference(setA, setB)
+console.log(setResult);
