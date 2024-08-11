736e30dc4f5d0bdbac453423b53d2644333bcfc4
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-11 15:06:38 +0530

    Spread and Rest Operator

--- Module43/Destructuring.js
+++ Module43/Destructuring.js
@@ -2,10 +2,19 @@
 let arrOne = [1,2,3]
 let arrTwo = [4,5,6]
 let arrThird = [...arrOne,...arrTwo]
-console.log(arrThird);
+// console.log(arrThird);
 
 //Rest
-function rest(...arguments){
-    console.log(...arguments);
+function rest(){
+    let arr = Array.from(arguments)
+    let result = arr.map(e => e)
+    console.log(result);
 }
 rest(1,2,3,4,5)
+
+
+function rest2(...args){
+    let result = args.map(e => e)
+    console.log(result);
+}
+rest2(1,2,3,4,5)
