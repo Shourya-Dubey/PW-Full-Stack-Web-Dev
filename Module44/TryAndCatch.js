8a7ecf6be7497dd0cacc12ab5cbb2a00272086ac
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-12 16:49:49 +0530

    TryCatch Demonstration

--- Module44/TryAndCatch.js
+++ Module44/TryAndCatch.js
@@ -1,13 +1,25 @@
-try{
-    const a = undefined
-    console.log(a[0])
-}
-catch(err){
-    console.log('error block executed')
-    console.log(typeof err);
-}
-finally{
-    console.log('Finally Executed')
-}
+// try{
+//     const a = undefined
+//     console.log(a[0])
+// }
+// catch(err){
+//     console.log('error block executed')
+//     console.log(typeof err);
+// }
+// finally{
+//     console.log('Finally Executed')
+// }
 
-console.log('end');
+// console.log('end');
+
+
+try{
+    console.log("Starting Try")
+    console.log(a)
+    console.log("ending Try")
+}catch(err){
+    console.log("handled",err)
+}finally{
+    console.log("finally")
+}
+console.log('end')
