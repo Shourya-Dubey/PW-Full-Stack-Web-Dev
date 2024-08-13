71eda35c7aa24c2c708548a5ff24d312ea99514c
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-13 18:02:33 +0530

    LexicalScopping

--- Module44/closer.js
+++ Module44/closer.js
@@ -23,16 +23,42 @@
 // console.log(score);
 
 
-function outerFunc(){
+//Lexical Scopping
+// function outerFunc(){
 
-    let outerVar = 'i am at scope level-1'
+//     let outerVar = 'i am at scope level-1'
+//     function innerFunc(){
+//         let innerVar = "i am at scope lever-2";
+//         console.log(outerVar);
+//     }
+//     innerFunc()
+//     console.log(innerVar);
+    
+// }
+
+// outerFunc()
+
+
+
+
+let GlobalVal = 0
+
+function func(){
+    const var1 = 1
+    console.log(GlobalVal)
+
     function innerFunc(){
-        let innerVar = "i am at scope lever-2";
-        console.log(outerVar);
+        const var2 = 2
+        console.log(var2 , var1, GlobalVal);
+
+        function innerOfinnerfunc(){
+            const var3 = 3
+            console.log(var3, var2, var1, GlobalVal);
+        }
+        
+        innerOfinnerfunc()
     }
     innerFunc()
-    console.log(innerVar);
-    
 }
 
-outerFunc()
+func()
