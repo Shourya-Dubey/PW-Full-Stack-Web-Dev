09272a2206933f3f1a86a0dc3a6407ca7a67445e
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-16 16:00:02 +0530

    setup create react app and intro of JSX

---
+++ Module66-React/reactapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button id='btn'>
+        <span>
+          submit
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default App;
