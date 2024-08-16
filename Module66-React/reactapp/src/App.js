4c09414b1072af148b1000642388691a552f3d8f
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-16 17:35:03 +0530

    Fragments

--- Module66-React/reactapp/src/App.js
+++ Module66-React/reactapp/src/App.js
@@ -2,11 +2,11 @@ import DogCard1 ,{Image} from "./DogCard";
 
 function App() {
   return (
-    <div className="App">
+    <>
      <DogCard1/>
      <DogCard1/>
      <Image/>
-    </div>
+    </>
   );
 }
 
