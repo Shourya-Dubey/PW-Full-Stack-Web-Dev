9d7d9813dceb9fb77b2da70b2398c6addc976f8c
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-16 19:02:09 +0530

    Passing JSX code as Children

--- Module66-React/reactapp/src/DogCard.js
+++ Module66-React/reactapp/src/DogCard.js
@@ -1,4 +1,5 @@
 import Image from "./Image";
+import Name from "./Name";
 
 // export function Image(props){
 //     return (
@@ -8,10 +9,21 @@ import Image from "./Image";
 
 
 function DogCard (props){
+  let title = 'this is title'
     return (
       <div>
-        <h3>{props.name}</h3>
-        <Image src={props.image}/>
+        <h2 style={
+            {
+             fontSize: "10px",
+             color: "red"
+            }
+          }>
+          {title}
+        </h2>
+        <Name>
+          <h3>{props.name}</h3>
+        </Name>
+        <Image src={props.image} />
       </div>
     );
 }
