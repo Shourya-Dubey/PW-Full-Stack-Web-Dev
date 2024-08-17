70fbbf4a5018a8a85b4f938a8479fcc46210593f
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-17 18:49:17 +0530

    Tic-Tac-Toe UI

---
+++ Module66-React/Tic-Tac-Toe/Tic-Tac-Toe/src/components/Icon/Icon.jsx
@@ -0,0 +1,14 @@
+import { FaCircle, FaPen, FaTimes } from "react-icons/fa";
+
+function Icon({name}){
+    if(name == 'circle'){
+        return <FaCircle/>
+    }
+    else if(name == 'cross'){
+        return <FaTimes/>
+    }
+    else{
+        return <FaPen/>
+    }
+}
+export default Icon;
