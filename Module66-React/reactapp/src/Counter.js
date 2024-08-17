006057ed93ae14504c27396bfb74e72f885e8775
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-17 14:03:20 +0530

    isEditting

--- Module66-React/reactapp/src/Counter.js
+++ Module66-React/reactapp/src/Counter.js
@@ -7,6 +7,7 @@ function Counter(){
     const[x, setX] = useState(0);
     const[y, setY] = useState(0);
     const[a, setA] = useState(0);
+    const[isEditting, setIsEditting] = useState(false);
 
     return (
       <>
@@ -37,6 +38,12 @@ function Counter(){
         count A: {a} is {a % 2 == 0 ? "Even" : "Odd"}
         <button onClick={() => setA(a + 1)}>Increment</button>
         <button onClick={() => setA(a - 1)}>Decrement</button>
+        <br/>
+
+        {
+         isEditting ? <input /> : <p>Shourya Dubey</p>
+        }
+        <button onClick={() => setIsEditting(!isEditting)}>Click</button>
       </>
     );
 }
