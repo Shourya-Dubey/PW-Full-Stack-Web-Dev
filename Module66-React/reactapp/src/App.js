592fb7fd1fac2dc59a300225f313b5e91ea4c74d
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-17 12:34:15 +0530

    useState Hook

--- Module66-React/reactapp/src/App.js
+++ Module66-React/reactapp/src/App.js
@@ -1,10 +1,11 @@
 import Image from "./Image";
 import DogCard1  from "./DogCard";
+import Counter from "./Counter";
 
 function App() {
   return (
     <>
-      Hello
+      {/* Hello
       <DogCard1
         name="Shourya"
         image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAnI33utT_AfatFvwqr9LLt599wJoIriqyIw&s"
@@ -13,7 +14,10 @@ function App() {
         name="Akash"
         image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW7zpG7Q2QGK9YGchMgfbXycRtDimjSCjk8w&s"
       />
-      <Image image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLm-r3QBCn15pn9zz68VFG1Bw2keUrwZ7WueGL4rQuA2TQpQA9jOrEt4nDisDresZIMIM&usqp=CAU" />
+      <Image image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLm-r3QBCn15pn9zz68VFG1Bw2keUrwZ7WueGL4rQuA2TQpQA9jOrEt4nDisDresZIMIM&usqp=CAU" /> */}
+
+      <Counter/>
+
     </>
   );
 }
---
+++ Module66-React/reactapp/src/Counter.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+let z = 0;
+
+function Counter(){
+    
+    const[x, setX] = useState(0);
+    const[y, setY] = useState(0);
+    const[a, setA] = useState(0);
+
+    return (
+        <>
+            count z = {z}
+            <button onClick={()=>{z+= 1}}>zIncrement</button>
+            <button onClick={()=>{z-= 1}}>zDecrement</button>
+            <br/>
+
+            count x : {x}
+            <button onClick={()=>setX(x+1)}>Increment</button>
+            <button onClick={()=>setX(x-1)}>Decrement</button>
+            <br/>
+
+            count y : {y}
+            <button onClick={()=>setY(y+1)}>Increment</button>
+            <button onClick={()=>setY(y-1)}>Decrement</button>
+            <br/>
+
+            count A: {a} is {(a%2 == 0) ? 'Even': 'Odd'}
+            <button onClick={()=>setA(a+1)}>Check</button>
+            
+        </>
+    )
+}
+export default Counter
